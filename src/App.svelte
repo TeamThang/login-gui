0b9432fc588932395a20a9d8c79a1fbae8bd5771
yuanzhaoyi <yuanzhaoyi@outlook.com>
2020-06-15 00:38:37 +0800

    INIT

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+    import LoginCard from "./Login.svelte";
+</script>
+
+<main>
+	<h1>瓜瓜局！</h1>
+  	<section>
+    	<LoginCard />
+  	</section>
+</main>
+
+<style>
+  .card {
+    text-align: left;
+    border-radius: 5px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 10px;
+  }
+
+  h1 {
+    font-size: 20px;
+  }
+
+  h2 {
+    font-size: 15px;
+    color: #999;
+  }
+
+  button {
+    background-color: #ff3e00;
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+    border: none;
+  }
+</style>
---
+++ src/Login.svelte
@@ -0,0 +1,7 @@
+// src/Login.svelte
+<div class='card'>
+  <h1>title</h1>
+  <div>price</div>
+  <div>description</div>
+  <button>登录</button>
+</div>
